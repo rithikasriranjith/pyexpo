9a5ef2a64cf17702d0c8928d4b48dc184464f857
rithikasriranjith <rithikasri152329@gmail.com>
2026-01-29 11:53:27 +0530

    app.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("WELCOME TO PYTHON")
+import streamlit as st
+import pandas as pd
+st.title("Student Management System")
+st.subheader("Enter Studeent Details")
+name=st.text_input("Enter Name:")
+age=st.number_input("Enter Age:")
+st.button("Submit")
+stu_list={"Name":name,"Age",age}
+st.dataframe(stu_list)
